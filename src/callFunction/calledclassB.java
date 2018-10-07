6fa0e6ccefcac550c4a96525764888702d6e4867
contact4harish <contact4harish@gmail.com>
2018-10-07 01:35:19 -0500

    java oops initial commit

---
+++ src/callFunction/calledclassB.java
@@ -0,0 +1,16 @@
+package callFunction;
+
+public class calledclassB {
+
+	
+	public int ReturnMethod(){
+		
+		return 10;
+		//no argument with return value
+				
+	}
+	public String ReturnMethod2(){
+		
+		return "Haris123";
+	}
+}
